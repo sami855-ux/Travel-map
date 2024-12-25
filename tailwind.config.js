ef0d61c38824c7e46305a0fe29a9b20ac8de2b7e
sami855-ux <samitale56@outlook.com>
2024-12-25 16:22:12 +0500

    New color is added with its gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       fontFamily: {
         Lato: ["Lato", "sans-serif"],
       },
+      colors: {
+        secondary: {
+          100: "#0454e7",
+          200: "#0749c3",
+          300: "#0741ad",
+          400: "#053896",
+          500: "#042e7c",
+          600: "#032058",
+        },
+      },
     },
   },
   plugins: [],
